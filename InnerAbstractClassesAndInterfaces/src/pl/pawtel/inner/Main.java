b0805be6d34179f98d36be9b25dffef4532648e1
Lipien <adam@pawtel.pl>
2020-06-26 14:10:14 +0200

    Explanation of inner class instantiation; comments

--- InnerAbstractClassesAndInterfaces/src/pl/pawtel/inner/Main.java
+++ InnerAbstractClassesAndInterfaces/src/pl/pawtel/inner/Main.java
@@ -3,7 +3,12 @@ package pl.pawtel.inner;
 public class Main {
 
     public static void main(String[] args){
-
+        Gearbox mcLaren = new Gearbox(6);
+        //to create instance of inner class have to have instance of outer class
+        // until inner class is static you can't use Gearbox.Gear syntax
+        //has to be instance.new, means mcLaren.new
+        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+        System.out.println(first.driveSpeed(1000));
     }
 
 
