0a069fc6e02af9e38655f69079c973e8b258b07a
Lipien <adam@pawtel.pl>
2020-06-27 09:40:27 +0200

    addGear put into constructor

--- InnerAbstractClassesAndInterfaces/src/pl/pawtel/inner/Main.java
+++ InnerAbstractClassesAndInterfaces/src/pl/pawtel/inner/Main.java
@@ -2,14 +2,17 @@ package pl.pawtel.inner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Gearbox mcLaren = new Gearbox(6);
-        //to create instance of inner class have to have instance of outer class
-        // until inner class is static you can't use Gearbox.Gear syntax
-        //has to be instance.new, means mcLaren.new
-        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
-        System.out.println(first.driveSpeed(1000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
     }
-
-
 }
