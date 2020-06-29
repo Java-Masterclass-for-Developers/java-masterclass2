acf6874bac00d864676eafb1679f5f75c29f7445
Lipien <adam@pawtel.pl>
2020-06-29 13:01:25 +0200

    anonymous class

--- InnerAbstractClassesAndInterfaces/src/pl/pawtel/inner/Main.java
+++ InnerAbstractClassesAndInterfaces/src/pl/pawtel/inner/Main.java
@@ -8,17 +8,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        class ClickListener implements Button.OnClickListener {
-            public ClickListener() {
-                System.out.println("I've beet attached");
-            }
-
+//        class ClickListener implements Button.OnClickListener {
+//            public ClickListener() {
+//                System.out.println("I've beet attached");
+//            }
+//
+//            @Override
+//            public void onClick(String title) {
+//                System.out.println(title + " was clicked");
+//            }
+//        }
+//        btnPrint.setOnClickListener((new ClickListener()));
+        btnPrint.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(String title) {
-                System.out.println(title + " was clicked");
+                System.out.println("Title was clicked");
             }
-        }
-        btnPrint.setOnClickListener((new ClickListener()));
+        });
         listen();
     }
 
